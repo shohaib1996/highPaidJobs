f026fef93fa5862c3f1422b7212291f28b9fd2ee
Shohaib hossain <khanshohaibhossain@gmail.com>
2025-03-24 01:12:53 +0600

    done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,12 +9,32 @@ import NewPath from "@/components/NewPath/NewPath";
 import RecentOffer from "@/components/offer/RecentOffer";
 import PricingSection from "@/components/pricingSection/PricingSection";
 import TicketToSuccess from "@/components/ticketToSuccess/TicketToSuccess";
-
 import WhyChooseUs from "@/components/whyChooseUs/WhyChooseUs";
+
+import Head from "next/head";
 
 export default function Home() {
   return (
     <>
+      <Head>
+        {/* Basic Meta Tags */}
+        <title>High Paid Jobs</title>
+        <meta name="description" content="Join us for the best learning experience and high-paying career opportunities!" />
+        
+        {/* Open Graph Meta Tags (for social media preview) */}
+        <meta property="og:title" content="High Paid Jobs" />
+        <meta property="og:description" content="Join us for the best learning experience and high-paying career opportunities!" />
+        <meta property="og:image" content="https://www.highpaidjobs.us/high-paid-jobs.png" />
+        <meta property="og:url" content="https://www.highpaidjobs.us/" />
+        <meta property="og:type" content="website" />
+
+        {/* Twitter Meta Tags (for Twitter preview) */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="High Paid Jobs" />
+        <meta name="twitter:description" content="Join us for the best learning experience and high-paying career opportunities!" />
+        <meta name="twitter:image" content="https://www.highpaidjobs.us/high-paid-jobs.png" />
+      </Head>
+
       <Hero />
       <TicketToSuccess />
       <HighPay />
